1c0ea85befec46c38214384d82dc408edb27931f
koba-masa <kobayashi_mshr@koba-masa.com>
2023-04-26 22:49:43 +0900

    feat: パスエイリアスを設定

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,7 @@
 import { createApp } from 'vue'
 import './style.css'
-import App from './App.vue'
-import { worker } from './mocks/browser'
+import App from '@/App.vue'
+import { worker } from '@/mocks/browser'
 
 if (process.env.NODE_ENV === 'development') {
   worker.start()
