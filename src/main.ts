77f52ccaba2621af988e7ae05d69b1a94e61afe3
koba-masa <kobayashi_mshr@koba-masa.com>
2023-04-30 00:29:45 +0900

    fix: Prettierの指摘対応

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,11 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from '@/App.vue'
-import { worker } from '@/mocks/browser'
+import { createApp } from 'vue';
+import './style.css';
+import App from '@/App.vue';
+import { worker } from '@/mocks/browser';
 import router from '@/router';
 
 if (process.env.NODE_ENV === 'development') {
-  worker.start()
+  worker.start();
 }
 
-createApp(App)
-  .use(router)
-  .mount('#app')
+createApp(App).use(router).mount('#app');
--- src/pages/ImageList.vue
+++ src/pages/ImageList.vue
@@ -1,13 +1,11 @@
 <script setup lang="ts">
-
 import { UploadImage as UploadImageType } from '@/models/UploadImage';
 import UploadImage from '@/components/images/UploadImage.vue';
 
 const images: Array<UploadImageType> = [
-  {id: 1, memo: 'iamge_001', path: 'https://placehold.jp/150x150.png'},
-  {id: 2, memo: 'iamge_002', path: 'https://placehold.jp/150x150.png'}
+  { id: 1, memo: 'iamge_001', path: 'https://placehold.jp/150x150.png' },
+  { id: 2, memo: 'iamge_002', path: 'https://placehold.jp/150x150.png' },
 ];
-
 </script>
 
 <template>
